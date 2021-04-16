c8787310f9564f3ba761c0db619147375206753f
christianvch <57494800+christianvch@users.noreply.github.com>
2021-04-16 01:31:32 -0500

    Update buildJavascriptApp.groovy

--- vars/buildJavascriptApp.groovy
+++ vars/buildJavascriptApp.groovy
@@ -1,18 +1,18 @@
 def call(Map config=[:], Closure body) {
     node {
-        git url: "https://github.com/werne2j/sample-nodejs"
+        git url: "https://github.com/jabedhasan21/java-hello-world-with-maven.git"
+
+        stage("Compile") {
+            sh "mvn compile"
+        }
+
+        stage("Package") {
+            sh "mvn package"
+        }
 
         stage("Install") {
-            sh "npm install"
-        }
-
-        stage("Test") {
-            sh "npm test"
-        }
-
-        stage("Deploy") {
             if (config.deploy) {
-                sh "npm publish"
+                sh "mvn install"
             }
         }
 
