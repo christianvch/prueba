6dd41370423058b5473de13955b74c8bcb0be075
christianvch <57494800+christianvch@users.noreply.github.com>
2021-04-18 00:42:11 -0500

    Update Constants.groovy

--- src/org/example/Constants.groovy
+++ src/org/example/Constants.groovy
@@ -1,6 +1,1 @@
-package org.example
 
-class Constants {
-  public static final String SLACK_MESSAGE = "Sending Slack Notification..."
-  public static final String EMAIL_MESSAGE = "Sending Email..."
-}
